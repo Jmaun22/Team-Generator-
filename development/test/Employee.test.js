e6a80cee40b2ae18638bf70ba939df7d82b4cb04
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-24 18:19:25 -0400

    did employee tests

--- development/test/Employee.test.js
+++ development/test/Employee.test.js
@@ -12,7 +12,15 @@ describe('Employee', () => {
 
     it('Should return the employees id', () => {
 
-        const 
+        const employee = new Employee('name', 'id', 'email');
+
+        expect(employee.getId()).toEqual('id');
+    })
+    it("Should return the email of the employee", () => {
+
+        const employee = new Employee('name', 'id', 'email');
+        
+        expect(employee.getEmail()).toEqual('email')
     })
 
 
